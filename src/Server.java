5b446c10451d8bf30259a90ffce111fa96a53192
Horia Apostol <apostol_dan1901@yahoo.com>
2023-03-21 16:35:20 +0200

    added shutdown

--- src/Server.java
+++ src/Server.java
@@ -8,6 +8,31 @@ import java.util.ArrayList;
 public class Server {
 
 	private ArrayList<SocketHandler> connections;
+	private ServerSocket server;
+	private boolean active;
+
+	public Server() {
+		this.connections = new ArrayList<>();
+		active = false;
+
+	}
+
+	public void shutDown() {
+		try {
+
+			active = true;
+			
+			for(SocketHandler sh:connections) {
+				sh.shutDown();
+			}
+			
+			if (server.isClosed() == false) {
+				server.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void broadcast(String msg) {
 		for (SocketHandler sh : connections) {
@@ -17,16 +42,20 @@ public class Server {
 
 	public void run() {
 		try {
-			ServerSocket server = new ServerSocket(9999);
-			Socket client = server.accept();
-			SocketHandler handler = new SocketHandler(client);
-			connections.add(handler);
+			server = new ServerSocket(9999);
+
+			while (!active) {
+				Socket client = server.accept();
+				SocketHandler handler = new SocketHandler(client);
+				connections.add(handler);
+			}
 		} catch (Exception e) {
 
 		}
 	}
 
 	public class SocketHandler {
+
 		private Socket client;
 		private BufferedReader in;
 		private PrintWriter out;
@@ -46,11 +75,32 @@ public class Server {
 				username = in.readLine();
 				System.out.println(username + " connected!Feel free to talk...");
 				broadcast(username + " joined!");
+				String message;
+				while ((message = in.readLine()) != null) {
+					if (message.startsWith("/quit")) {
+
+					} else {
+						broadcast(username + ": " + message);
+					}
+				}
+
 			} catch (Exception e) {
 
 			}
 		}
 
+		public void shutDown() {
+			try {
+			out.close();
+			in.close();
+			if(!client.isClosed()) {
+				client.close();
+			}
+		} catch (Exception e) {
+				
+			}
+		}
+		
 		public void writeMessage(String msg) {
 			out.println(msg);
 		}
